14809765ee98f9d98fa77c255a3df027d1d3e593
Samuel Kim <samuel.kim@clover.com>
2017-06-21 23:01:46 -0700

    init

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: {
+    path: path.resolve(__dirname, 'frontend', 'simplesync.jsx')
+  },
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+}
