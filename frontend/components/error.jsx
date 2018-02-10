f1a842b475de05ae6ead83eaba64aee164756a5c
Samuel Kim <samuel.kim@clover.com>
2018-02-10 00:11:43 -0800

    working on invite button, create user, send email

--- frontend/components/error.jsx
+++ frontend/components/error.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
+import React from 'react';
+import {createTodo} from '../actions/todo_actions';
 
 export const ErrorList = ({errors, clearErrors}) => {
   if(errors.length === 0) return null;
   const errorItems = Object.values(errors).map(error => <li key={ error }>{ error }</li>);
 
+  function handleInviteUser() {
+    const todo = {
+      owner_id: this.props.user.current_user.id,
+      title: "",
+      body: "",
+      done: false
+    };
+    createTodo({todo});
+  }
+
   return(
     <ul className="error-list" style={{display:'flex', flexDirection:'row'}}>
       {errorItems}
       <div>
-        <input type="button" name="invite" value="invite"/>
+        <input type="button" name="invite" value="invite" onClick={handleInviteUser}/>
         <input type="button" name="close" value="close" onClick={clearErrors} />
       </div>
     </ul>
   )
+
 };
