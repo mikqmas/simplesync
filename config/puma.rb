36295ac794d4c5d9424513b3717a722dddceb271
Samuel Kim <samuel.kim@clover.com>
2018-02-10 23:07:29 -0800

    puma support

--- config/puma.rb
+++ config/puma.rb
@@ -45,3 +45,9 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+
+
+# added for AWS support
+bind "unix:///var/run/puma/my_app.sock"
+pidfile "/var/run/puma/my_app.sock"
