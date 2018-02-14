9fb4e28bfd699da96b2100f17faeef7f7a729b1c
Samuel Kim <samuel.kim@clover.com>
2018-02-13 23:26:22 -0800

    script

---
+++ webpack.config.prod.js
@@ -0,0 +1,39 @@
+var path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  context: __dirname,
+  path: path.resolve(__dirname, 'frontend', 'entry.jsx'),
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'cheap-module-source-map',
+  resolve: {
+    extensions: ["", ".js", ".jsx" ]
+  }
+};
